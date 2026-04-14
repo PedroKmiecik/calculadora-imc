aedfaa1afd8899e16749844957ff3307efe8d798
pedro kmiecik <pedro.kmiecik@gmail.com>
2026-04-14 19:49:23 -0300

    adiciona entrada de nome, peso e altura

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 # Calculadora de IMC
 print("Bem-vindo à Calculadora de IMC!")
+
+nome = input("Digite seu nome: ")
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite sua altura (m): "))
